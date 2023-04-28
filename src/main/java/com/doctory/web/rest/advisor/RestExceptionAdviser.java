1d0f6b4bd1322ed977c7fe1ad57657dd1525d0f6
Sagir <sagir.md@sil.mu>
2023-04-28 11:37:04 +0400

    Hospital module api test

--- src/main/java/com/doctory/web/rest/advisor/RestExceptionAdviser.java
+++ src/main/java/com/doctory/web/rest/advisor/RestExceptionAdviser.java
@@ -10,11 +10,11 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestControllerAdvice
 public class RestExceptionAdviser {
