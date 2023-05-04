68b7ec16f81f30e04d89e3be75f1446e8ad36b97
Sagir <sagir.md@sil.mu>
2023-05-04 11:49:29 +0400

    branch service test are passed

--- src/main/java/com/doctory/web/request/DoctorRequest.java
+++ src/main/java/com/doctory/web/request/DoctorRequest.java
@@ -2,13 +2,20 @@ package com.doctory.web.request;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record DoctorRequest(
-        @NotBlank(message = "The hospital id must be defined")
+        @NotNull(message = "The branch id must be defined")
+        @Positive(message = "The branch id must be positive no")
         Long branchId,
+        @NotBlank(message = "The doctor name must be defined")
         String doctorName,
+        @NotBlank(message = "The doctor degree must be defined")
         String doctorDegree,
+        @NotBlank(message = "The doctor practice must be defined")
         String practiceName,
+        @NotBlank(message = "The doctor specialist must be defined")
         String specialist,
         @Valid
         AddressRequest addressRequest,
--- src/main/java/com/doctory/web/request/PersonRequest.java
+++ src/main/java/com/doctory/web/request/PersonRequest.java
@@ -11,4 +11,7 @@ public record PersonRequest(
         String mobileNo,
         String alternateNo,
         String email) {
+        public static PersonRequest of(String firstName, String lastName, String surname, String mobileNo, String alternateNo, String email) {
+                return new PersonRequest(firstName,lastName,surname,mobileNo,alternateNo,email);
+        }
 }
